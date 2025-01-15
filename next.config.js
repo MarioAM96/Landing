36145e185411bed7d7836e1df72494536ba4c1d8
Mario Muñoz <mario.munoz@fibramax.ec>
2025-01-15 08:59:30 -0500

    Configuracion para Build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/landing' //Path Base para produccion
+};
 
 module.exports = nextConfig;
